5696faf823326e66727020faa829747b1e6f02d5
io4-Nakul <122207464+io4-Nakul@users.noreply.github.com>
2023-03-29 23:02:21 +0530

    Add extra program

--- Assignment/2023-03-17 javascript-3 programs/script.js
+++ Assignment/2023-03-17 javascript-3 programs/script.js
@@ -21,9 +21,10 @@ while (num > 0) {
 console.log("The sum of the digits is " + sum);
 
 //3
-const number = prompt("12345: ");
-const reversedNumber = Number(number.toString().split('').reverse().join(''));
-console.log(`Reversed number: ${reversedNumber}`);
+let number = prompt("12345:");
+let reversedNumber = number.split("").reverse().join("");
+
+console.log(`The reversed number is ${reversedNumber}`);
 
 //4
 let A = parseFloat(prompt("10:"));
@@ -34,17 +35,152 @@ let Area = Math.sqrt(S * (S - A) * (S - B) * (S - C));
 
 console.log("The area of the triangle is " +Area.toFixed(2));
 
-//5
-let x = parseFloat(prompt("Enter the x-coordinate: "));
-let y = parseFloat(prompt("Enter the y-coordinate: "));
+//5 Did not get it
+//6 Did not get it
+//7 Did not get it
 
-let r = Math.sqrt(x ** 2 + y ** 2);
-let phi = Math.atan2(y, x) - (9 * Math.PI / 180);
+//8
+const Number = parseInt(prompt("20: "));
 
-console.log(`Cartesian co-ordinates: (${x}, ${y})`);
-console.log(`Polar co-ordinates: (r=${r}, φ=${phi})`);
+if (number % 2 === 0) {
+  console.log(`${number} is an even number.`);
+} else {
+  console.log(`${number} is an odd number.`);
+}
 
-//6
+//9
+let year = prompt("2024:");
+if (year % 4 === 0 && (year % 100 !==0 || year % 400 === 0)) {
+    console.log(year + "is a leap year.");} 
+    else {
+        console.log(year + "is not a leap year.");
+    }
+
+    //10
+ let basicSalary = prompt("4500:");
+
+let hra, da, grossSalary;
+
+if (basicSalary < 2000) {
+  hra = 0.1 * basicSalary;
+  da = 0.9 * basicSalary;
+} else {
+  hra = 500;
+  da = 0.98 * basicSalary;
+}
+grossSalary = parseFloat(basicSalary) + parseFloat(hra) + parseFloat(da);
+
+console.log(`The employee's gross salary is Rs. ${grossSalary}`);
+
+//11
+let ramAge = prompt("Ram's age 11: ");
+let shyamAge = prompt("Shyam's age 12: ");
+let ajayAge = prompt("Ajay's age 13: ");
+
+ramAge = parseInt(ramAge);
+shyamAge = parseInt(shyamAge);
+ajayAge = parseInt(ajayAge);
+
+
+let youngestAge = ramAge;
+
+if (shyamAge < youngestAge) {
+  youngestAge = shyamAge;
+}
+
+if (ajayAge < youngestAge) {
+  youngestAge = ajayAge;
+}
+
+console.log("The youngest of the three is " + youngestAge + " years old.");
+
+//12
+let angle1 = parseInt(prompt("Enter the first angle of the triangle:"));
+let angle2 = parseInt(prompt("Enter the second angle of the triangle:"));
+let angle3 = parseInt(prompt("Enter the third angle of the triangle:"));
+
+if(angle1 + angle2 + angle3 == 180) {
+  console.log("The triangle is valid.");
+}
+else {
+  console.log("The triangle is not valid.");
+}
+
+//13 Did not get it
+
+//14
+const length = prompt("Enter the length of the rectangle:");
+const breadth = prompt("Enter the breadth of the rectangle:");
+
+const area1 = length * breadth;
+const perimeter = 2 * (parseInt(length) + parseInt(breadth));
+
+if (area1 > perimeter) {
+  console.log("The area of the rectangle is greater than its perimeter.");
+} else {
+  console.log("The area of the rectangle is less than or equal to its perimeter.");
+}
+
+//15
+let marks = [];
+let totalMarks = 0;
+
+// Input marks for 5 subjects
+for (let i = 1; i <= 5; i++) {
+  marks[i] = parseInt(prompt("Enter marks for subject " + i));
+  totalMarks += marks[i];
+}
+let percentage = (totalMarks / 500) * 100;
+
+if (percentage >= 60) {
+  console.log("First division");
+} else if (percentage >= 50 && percentage <= 59) {
+  console.log("Second division");
+} else if (percentage >= 40 && percentage <= 49) {
+  console.log("Third division");
+} else {
+  console.log("Fail");
+}
+
+//16 Did not get it
+//17 Did not get it
+
+
+// 18
+let num2 = prompt("Enter a five-digit number:");
+let reversedNum = num.toString().split("").reverse().join("");
+
+if (num2 == reversedNum) {
+  console.log("The original and reversed numbers are equal.");
+} else {
+  console.log("The original and reversed numbers are not equal.");
+}
+
+// 19
+let costPrice = parseFloat(prompt("Enter the cost price:"));
+let sellingPrice = parseFloat(prompt("Enter the selling price:"));
+
+// Calculate profit or loss
+let profitOrLoss = sellingPrice - costPrice;
+
+// Determine if seller made profit or incurred loss
+if (profitOrLoss > 0) {
+  console.log("Seller made a profit of " + profitOrLoss.toFixed(2));
+} else if (profitOrLoss < 0) {
+  console.log("Seller incurred a loss of " + (-profitOrLoss).toFixed(2));
+} else {
+  console.log("Seller broke even - no profit, no loss.");
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
