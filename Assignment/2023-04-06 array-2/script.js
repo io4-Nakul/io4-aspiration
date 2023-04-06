5a4162ab77bd2a725ef70c4cb23433d1f57a941c
io4-Nakul <122207464+io4-Nakul@users.noreply.github.com>
2023-04-06 22:45:52 +0530

    Assignment

---
+++ Assignment/2023-04-06 array-2/script.js
@@ -0,0 +1,32 @@
+//  2
+function isArray(input) {
+    return Array.isArray(input);
+  }
+  console.log(isArray('I am a string.')); 
+  console.log(isArray([1, 2, 4, 0, 'array'])); 
+
+
+  //  3 a
+  let arr3a = [5, 12, 8, 130, 44];
+  console.log(arr3a.length); 
+  //  3 b
+  let arr3b = [ 'g','e','e','k','s'];
+  console.log(arr3b.length);
+  //  3 c
+  let arr3c = ['shoes', 'shirts', 'socks', 'sweaters'];
+  console.log(arr3c.length);
+  //  3 d
+  let arr3d = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];
+  console.log(arr3d.length);
+
+
+  //  4 a
+
+const day = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];
+
+const dayOutput = day[3];
+
+console.log(dayOutput);
+
+
+    
