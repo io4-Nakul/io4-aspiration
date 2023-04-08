4d15054174774afeb0843ec3bebce02c7f80add3
io4-Nakul <122207464+io4-Nakul@users.noreply.github.com>
2023-04-08 22:49:22 +0530

    Add more

--- Assignment/2023-04-06 array-2/script.js
+++ Assignment/2023-04-06 array-2/script.js
@@ -28,5 +28,114 @@ const dayOutput = day[3];
 
 console.log(dayOutput);
 
+//  4 b
+const item = [ 'shoes', 'shirts', 'socks', 'sweaters' ];
+const itemOutput = item[3];
+console.log(itemOutput); 
+
+
+// 5 a
+let day1 = ['Sunday', 'Monday'];
+let day2 = ['Friday', 'Saturday'];
+let day3 = ['Tuesday', 'Wednesday', 'Thursday'];
+
+let concatOutput = day1.concat(day2,day3);
+console.log(concatOutput); 
+//  5 b
+let num = [5, 12, 8, 130, 44];
+let alfa = ['g','e','e','k','s'];
+let numOutput = num.concat(alfa);
+console.log(numOutput);
+
+
+//  6  
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+days.sort(); 
+console.log(days); 
+
+
+//  7  a
+let arr = [210, 'Sunday', 702, true, null, '90', 'vinay', 'CAT'];
+arr.reverse();
+console.log(arr);
+
+//  7 b
+let arr1 = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];
+arr1.reverse();
+console.log(arr1);
+
+//  8  a
+const arr8 = [210, 'Sunday', 702, true, null, '90', 'vinay', 'CAT'];
+
+const index1 = arr8.indexOf('90'); 
+const index2 = arr8.indexOf(true); 
+const index3 = arr8.indexOf('Ajay'); 
+
+console.log(index1); 
+console.log(index2); 
+console.log(index3); 
+
+//  8  b
+const num8 = [115, 54, 101, -52, -10, 0.56, 5, 12, 8, 130, 44];
+const index4 = num8.indexOf(0);         
+const index5 = num8.indexOf(-52);
+const index6 = num8.indexOf(56);
+
+console.log(index4);
+console.log(index5);
+console.log(index6);
+
+
+//  9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     
+
+
+
+
+
+
+
+
+
+
+
+
+
